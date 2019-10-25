161bb8ef55c5f733b0eaf47bd1d3b0ffe6a14cf7
jira <jira@jiras-MBP.fritz.box>
2019-10-25 21:24:49 +0200

    add initial cython test

---
+++ trajectory_planning_helpers/calc_splines_cy.pyx
@@ -0,0 +1,8 @@
+import numpy as np
+import math
+# "cimport" is used to import special compile-time information
+# about the numpy module (this is stored in a file numpy.pxd which is
+# currently part of the Cython distribution).
+cimport numpy as np
+
+print("Cython here")
