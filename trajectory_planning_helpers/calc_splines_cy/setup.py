31fab95690de3da837d978c650faac9c9bbf2d4e
jira <jira@jiras-MBP.fritz.box>
2019-10-25 22:33:52 +0200

    add cython test in subfolder

---
+++ trajectory_planning_helpers/calc_splines_cy/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from distutils.extension import Extension
+#from Cython.Build import cythonize
+from Cython.Distutils import build_ext as build_pyx
+import numpy
+
+# setup(
+#     ext_modules=[
+#         Extension("calc_splines_cy", ["calc_splines_cy.c"],
+#                   include_dirs=[numpy.get_include()]),
+#     ],
+# )
+
+# setup(
+#     ext_modules = cythonize("calc_splines_cy.pyx"),
+#     include_dirs=[numpy.get_include()]
+# )
+
+ext = [Extension('calc_splines_cy',
+                 sources=['calc_splines_cy.pyx'],
+                 include_dirs=[numpy.get_include()])]
+
+setup(name = 'calc_splines_cy', ext_modules=ext, cmdclass = { 'build_ext': build_pyx })
