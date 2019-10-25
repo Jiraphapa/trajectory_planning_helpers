167155d63e5bbc672eb257b1d202ff4e79196da2
jira <jira@jiras-MBP.fritz.box>
2019-10-25 21:29:44 +0200

    fix setup.py

--- trajectory_planning_helpers/setup.py
+++ trajectory_planning_helpers/setup.py
@@ -1,6 +1,7 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("calc_splines_cy.pyx")
+    ext_modules = cythonize("calc_splines_cy.pyx"),
+    include_dirs=[numpy.get_include()]
 )
