57cfb5584390c1cec5b6db2636b2c03cd7aaad8e
jira <jira@jiras-MBP.fritz.box>
2019-10-26 15:09:32 +0200

    assign cython datatype

--- trajectory_planning_helpers/calc_splines_cy/calc_splines_cy.pyx
+++ trajectory_planning_helpers/calc_splines_cy/calc_splines_cy.pyx
@@ -6,3 +6,150 @@ import math
 cimport numpy as np
 
 print("Cython here")
+
+# We now need to fix a datatype for our arrays. I've used the variable
+# DTYPE for this, which is assigned to the usual NumPy runtime
+# type info object.
+DTYPE = np.int
+# "ctypedef" assigns a corresponding compile-time type to DTYPE_t. For
+# every type in the numpy module there's a corresponding compile-time
+# type with a _t-suffix.
+ctypedef np.int_t DTYPE_t
+
+def calc_splines(path: np.ndarray,
+                 el_lengths: np.ndarray = None,
+                 psi_s: float = None,
+                 psi_e: float = None,
+                 use_dist_scaling: bool = True) -> tuple:
+                 
+     # check if path is closed
+    if np.all(np.isclose(path[0], path[-1])):
+        closed = True
+    else:
+        closed = False
+
+    # check inputs
+    if not closed and (psi_s is None or psi_e is None):
+        raise ValueError("Headings must be provided for unclosed spline calculation!")
+
+    if el_lengths is not None and path.shape[0] != el_lengths.size + 1:
+        raise ValueError("el_lengths input must be one element smaller than path input!")
+
+    # if distances between path coordinates are not provided but required, calculate euclidean distances as el_lengths
+    if use_dist_scaling and el_lengths is None:
+        el_lengths = np.sqrt(np.sum(np.power(np.diff(path, axis=0), 2), axis=1))
+
+    # if closed and use_dist_scaling active append element length in order to obtain overlapping scaling
+    if use_dist_scaling and closed:
+        el_lengths = np.hstack((el_lengths, el_lengths[0]))
+
+    # get number of splines
+    no_splines = path.shape[0] - 1
+
+    # calculate scaling factors between every pair of splines
+    if use_dist_scaling:
+        scaling = el_lengths[:-1] / el_lengths[1:]
+    else:
+        scaling = np.ones(no_splines - 1)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # DEFINE LINEAR EQUATION SYSTEM ------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+
+    # M_{x,y} * a_{x,y} = b_{x,y}) with a_{x,y} being the desired spline param
+    # *4 because of 4 parameters in cubic spline
+    M = np.zeros((no_splines * 4, no_splines * 4))
+    b_x = np.zeros((no_splines * 4, 1))
+    b_y = np.zeros((no_splines * 4, 1))
+
+    # create template for M array entries
+    template_M = np.array(                          # current time step           | next time step          | bounds
+                [[1,  0,  0,  0,  0,  0,  0,  0],   # a_0i                                                  = {x,y}_i
+                 [1,  1,  1,  1,  0,  0,  0,  0],   # a_0i + a_1i +  a_2i +  a_3i                           = {x,y}_i+1
+                 [0,  1,  2,  3,  0, -1,  0,  0],   # _      a_1i + 2a_2i + 3a_3i      - a_1i+1             = 0
+                 [0,  0,  2,  6,  0,  0, -2,  0]])  # _             2a_2i + 6a_3i               - 2a_2i+1   = 0
+
+    for i in range(no_splines):
+        j = i * 4
+
+        if i < no_splines - 1:
+            M[j: j + 4, j: j + 8] = template_M
+
+            M[j + 2, j + 5] *= scaling[i]
+            M[j + 3, j + 6] *= math.pow(scaling[i], 2)
+
+        else:
+            M[j: j + 2, j: j + 4] = [[1,  0,  0,  0],  # no curvature and heading bounds on last element
+                                     [1,  1,  1,  1]]
+
+        b_x[j: j + 2] = [[path[i,     0]],      # NOTE: the bounds of the two last equations remain zero
+                         [path[i + 1, 0]]]
+        b_y[j: j + 2] = [[path[i,     1]],
+                         [path[i + 1, 1]]]
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # SET BOUNDARY CONDITIONS FOR FIRST AND LAST POINT -----------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+
+    if not closed:
+        # We want to fix heading at the start and end point (heading and curvature at start gets unbound at spline end)
+        # heading and curvature boundary condition
+        M[-2, 1] = 1                  # heading start
+        M[-1, -4:] = [0,  1,  2,  3]  # heading end
+
+        # heading start
+        if el_lengths is None:
+            el_length_s = 1.0
+        else:
+            el_length_s = el_lengths[0]
+
+        b_x[-2] = math.cos(psi_s + math.pi / 2) * el_length_s
+        b_y[-2] = math.sin(psi_s + math.pi / 2) * el_length_s
+
+        # heading end
+        if el_lengths is None:
+            el_length_e = 1.0
+        else:
+            el_length_e = el_lengths[-1]
+
+        b_x[-1] = math.cos(psi_e + math.pi / 2) * el_length_e
+        b_y[-1] = math.sin(psi_e + math.pi / 2) * el_length_e
+
+    else:
+        # gradient boundary conditions (for a closed spline)
+        M[-2, 1] = scaling[-1]
+        M[-2, -3:] = [-1, -2, -3]
+        # b_x[-2] = 0
+        # b_y[-2] = 0
+
+        # curvature boundary conditions (for a closed spline)
+        M[-1, 2] = 2 * math.pow(scaling[-1], 2)
+        M[-1, -2:] = [-2, -6]
+        # b_x[-1] = 0
+        # b_y[-1] = 0
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # SOLVE ------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+
+    x_les = np.squeeze(np.linalg.solve(M, b_x))  # squeeze removes single-dimensional entries
+    y_les = np.squeeze(np.linalg.solve(M, b_y))
+
+    # get coefficients of every piece into one row -> reshape
+    coeffs_x = np.reshape(x_les, (no_splines, 4))
+    coeffs_y = np.reshape(y_les, (no_splines, 4))
+
+    # get normal vector (second coefficient of cubic splines is relevant for the gradient)
+    normvec = np.stack((coeffs_y[:, 1], -coeffs_x[:, 1]), axis=1)
+
+    # normalize normal vectors
+    norm_factors = 1.0 / np.sqrt(np.sum(np.power(normvec, 2), axis=1))
+    norm_factors = np.expand_dims(norm_factors, axis=1)  # second dimension must be inserted for next step
+    normvec_normalized = norm_factors * normvec
+
+    return coeffs_x, coeffs_y, M, normvec_normalized
+
+
+# testing --------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    pass
