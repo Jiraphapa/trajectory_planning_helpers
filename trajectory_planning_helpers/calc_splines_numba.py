d9373db37663f15ae8d488ca4284a47b4c49dbc3
Jiraphapa Jiravaraphan <j.jiravaraphan@tum.de>
2019-11-20 15:15:33 +0100

    add inputs checking, adapt np.diff to numba unsupported argument format

--- trajectory_planning_helpers/calc_splines_numba.py
+++ trajectory_planning_helpers/calc_splines_numba.py
@@ -6,7 +6,7 @@ from numba import jit, vectorize, float64
 @jit(nopython=True, cache=True)
 def isclose(a, b, rtol=1.e-5, atol=1.e-8, equal_nan=False):
     x, y = np.asarray(a), np.asarray(b)
-    # check if arrays are element-wise equal within a tolerance.
+    # check if arrays are element-wise equal within a tolerance (assume that both arrays are of valid format)
     result = np.less_equal(np.abs(x-y), atol + rtol * np.abs(y))   
     return result
 
@@ -22,6 +22,17 @@ def calc_splines(path: np.ndarray,
         closed = True
     else:
         closed = False
+
+    # check inputs
+    if not closed and (psi_s is None or psi_e is None):
+        raise ValueError("Headings must be provided for unclosed spline calculation!")
+
+    if el_lengths is not None and path.shape[0] != el_lengths.size + 1:
+        raise ValueError("el_lengths input must be one element smaller than path input!")
+
+    # if distances between path coordinates are not provided but required, calculate euclidean distances as el_lengths
+    if use_dist_scaling and el_lengths is None:
+        el_lengths = np.sqrt(np.sum(np.power(np.diff(path, 0), 2), axis=1))
 
    
     return 1,2,3,4
