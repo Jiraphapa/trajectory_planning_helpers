40c48a07a198f247f011de38fc7e5735880f4ef3
Jiraphapa Jiravaraphan <j.jiravaraphan@tum.de>
2019-11-28 11:41:04 +0100

    initial attempt to convert calc_splines into compiled AOT version

--- trajectory_planning_helpers/calc_splines_numba.py
+++ trajectory_planning_helpers/calc_splines_numba.py
@@ -7,6 +7,8 @@ from numba.pycc import CC
 cc = CC('my_module')
 #cc.verbose = True
 
+## ----------------------------------------------------------------------------------- ##  
+
 @cc.export('isclosef', 'boolean[:](f8[:], f8[:])')
 def isclosef(a, b):
     rtol, atol = 1.e-5, 1.e-8
@@ -16,11 +18,42 @@ def isclosef(a, b):
     return result
 
 @jit(nopython=True, cache=True)
-def isclose(a, b, rtol=1.e-5, atol=1.e-8, equal_nan=False):
+def isclose(a, b):
+    rtol, atol = 1.e-5, 1.e-8
     x, y = np.asarray(a), np.asarray(b)
     # check if arrays are element-wise equal within a tolerance (assume that both arrays are of valid format)
     result = np.less_equal(np.abs(x-y), atol + rtol * np.abs(y))   
     return result
+
+## ----------------------------------------------------------------------------------- ##    
+
+@cc.export('calc_splinesf', 'UniTuple(f8[:,:],4)(f8[:,:], f8[:], f8, f8, boolean)')
+def calc_splinesf(path: np.ndarray,
+                 el_lengths: np.ndarray = None,
+                 psi_s: float = None,
+                 psi_e: float = None,
+                 use_dist_scaling: bool = True) -> tuple:
+
+    # check if path is closed
+    if np.all(isclose(path[0], path[-1])):      # Numba 0.46.0 does not support NumPy function 'numpy.isclose'
+        closed = True
+    else:
+        closed = False
+
+    # check inputs
+    if not closed and (psi_s is None or psi_e is None):
+        raise ValueError("Headings must be provided for unclosed spline calculation!")
+
+    if el_lengths is not None and path.shape[0] != el_lengths.size + 1:
+        raise ValueError("el_lengths input must be one element smaller than path input!")
+
+    # if distances between path coordinates are not provided but required, calculate euclidean distances as el_lengths
+    # if use_dist_scaling and el_lengths is None:
+    #     el_lengths = np.sqrt(np.sum(np.power(np.diff(path, 0), 2), axis=1))
+
+    return np.ones((2,2)), np.ones((2,2)), np.ones((2,2)), np.ones((2,2))
+
+## ----------------------------------------------------------------------------------- ##  
 
 @jit(nopython=True, cache=True)
 def calc_splines(path: np.ndarray,
@@ -169,9 +202,11 @@ if __name__ == "__main__":
 cc.compile()
 
 path = np.ones((15,2))
-t = Timer(lambda: calc_splines(path))
+temp = np.ones((14))
+t = Timer(lambda: calc_splines(path, temp, 0.0, 0.0, True))
 print("Execution time for calc_splines with numba (with compilation):",t.timeit(number=1))
 
 path = np.ones((15,2))
-t = Timer(lambda: calc_splines(path))
+temp = np.ones((14))
+t = Timer(lambda: calc_splines(path, temp, 0.0, 0.0, True))
 print("Execution time for calc_splines with numba (after compilation):",t.timeit(number=1))
