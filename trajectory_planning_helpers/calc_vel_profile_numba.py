fix logic error in calculation of middle values of the convolution, refactor

trajectory_planning_helpers/calc_vel_profile_numba.py
@@ -8,10 +8,6 @@ from numba import jit
 # Module name
 cc = CC('calc_vel_profile_numba')
 
-@jit(nopython=True, cache=True)
-def test():
-    print(np.divide(1.0, np.zeros((5))))
-
 @cc.export('calc_vel_profile', 'float64[:](float64[:,:], float64[:], float64[:], boolean, float64, float64, float64, float64[:], float64, float64, int64)')
 @jit(nopython=True, cache=True)
 def calc_vel_profile(ggv: np.ndarray,
@@ -126,8 +122,8 @@ def calc_vel_profile(ggv: np.ndarray,
 
     if filt_window is not None:
         vx_profile = conv_filt_numba.conv_filt(signal=vx_profile,
-                                                                     filt_window=filt_window,
-                                                                     closed=closed)
+                                               filt_window=filt_window,
+                                               closed=closed)
 
     return vx_profile
 
@@ -263,6 +259,12 @@ def __solver_fb_closed(ggv: np.ndarray,
 def flipud(arr):
     return arr[::-1, ...]
 
+@jit(nopython=True, cache=True)
+def insert(array, index, value):
+    result_list = list(array.flatten())           
+    result_list.insert(index,value)
+    return np.asarray(result_list)
+
 @cc.export('__solver_fb_acc_profile', 'float64[:](float64[:,:], float64[:], float64[:], float64[:], float64[:], float64, float64, float64, boolean)')
 @jit(nopython=True, cache=True)
 def __solver_fb_acc_profile(ggv: np.ndarray,
@@ -285,8 +287,8 @@ def __solver_fb_acc_profile(ggv: np.ndarray,
     # check for reversed direction -> modify vx_profile, radii and el_lengths
     if backwards:
         col = np.asarray([0, 1, 3, 4])
-        ggv_mod = ggv[:, col]  # use negative acceleration in x axis if we are going backwards
-        radii_mod = flipud(radii)
+        ggv_mod = ggv[:, col]                # use negative acceleration in x axis if we are going backwards
+        radii_mod = flipud(radii)            # Notes: Numba 0.46.0 currently not support numpy.flipud
         el_lengths_mod = flipud(el_lengths)
         mu_mod = flipud(mu)
         vx_profile = flipud(vx_profile)
@@ -308,16 +310,11 @@ def __solver_fb_acc_profile(ggv: np.ndarray,
     if acc_inds.size != 0:
         # check index diffs -> we only need the first point of every acceleration phase
         acc_inds_diffs = np.diff(acc_inds)
-        #acc_inds_diffs = insert(acc_inds_diffs, 0, 2)    # Numba 0.46.0 currently not support numpy.insert
-        # TODO: extract into insert function    
-        acc_inds_diffs_ = list(acc_inds_diffs.flatten())           # first point is always a starting point
-        acc_inds_diffs_.insert(0,2)
-        acc_inds_diffs = np.asarray(acc_inds_diffs_)
-        acc_inds_rel = list(acc_inds[acc_inds_diffs > 1])         # starting point indices for acceleration phases
+        acc_inds_diffs = insert(acc_inds_diffs, 0, 2)       # Notes: Numba 0.46.0 currently not support numpy.insert
+        acc_inds_rel = list(acc_inds[acc_inds_diffs > 1])   # starting point indices for acceleration phases
     else:
-        #acc_inds_rel = []                                   # if vmax is low and can be driven all the time
-        # define empty list, but instruct that the type is np.float64(x) to avoid Numnba untyped list problem
-        acc_inds_rel = [np.int64(x) for x in range(0)]
+        # define empty list, instruct the type np.int64(x) to avoid Numba untyped list problem
+        acc_inds_rel = [np.int64(x) for x in range(0)]      # if vmax is low and can be driven all the time
         
     # ------------------------------------------------------------------------------------------------------------------
     # CALCULATE VELOCITY PROFILE ---------------------------------------------------------------------------------------

trajectory_planning_helpers/conv_filt_numba.py
@@ -6,17 +6,17 @@ from numba import jit
 cc = CC('conv_filt_numba')
 
 # Only return the middle values of the convolution. Contains boundary effects, where zeros are taken into account:
-# returns output of length max(m, n).
+# returns output of length max(n1, n2).
 @jit(nopython=True)
-def get_middle_values(array ,m, n):
-    if m > n:
-        m, n = n, m
-    length = m
-    n = n
-    n_left = n//2
+def get_middle_values(array, n1, n2):
+    if n1 < n2:
+        n1, n2 = n2, n1
+    n = n2
+    n_left = int(n/2)
     n_right = n - n_left - 1;
     return array[n_left:-n_right]
 
+@cc.export('conv_filt', 'float64[:](float64[:], int64, boolean)')
 @jit(nopython=True)
 def conv_filt(signal: np.ndarray,
               filt_window: int,
@@ -59,11 +59,10 @@ def conv_filt(signal: np.ndarray,
         # apply convolution filter used as a moving average filter and remove temporary points
         # Notes: Numba 0.46.0 currently support numpy.convolve with only 2 first arguments, 
         signal_filt = np.convolve(signal_tmp,
-                                  np.ones(filt_window) / float(filt_window)
-                                  )[w_window_half:-w_window_half]
+                                  np.ones(filt_window) / float(filt_window))
 
         # get_middle_values function works equivalent to adding 'mode="same"' argument in numpy.convolve
-        signal_filt = get_middle_values(signal_filt, signal_tmp.shape[0], filt_window)
+        signal_filt = get_middle_values(signal_filt, signal_tmp.shape[0], filt_window)[w_window_half:-w_window_half]
 
     else:
         # implementation 1: include boundaries during filtering
@@ -85,10 +84,10 @@ def conv_filt(signal: np.ndarray,
 
         signal_filt_tmp = np.convolve(signal,
                                       np.ones(filt_window) / float(filt_window)
-                                      )[w_window_half:-w_window_half]
+                                      )
 
         # get_middle_values function works equivalent to adding 'mode="same"' argument in numpy.convolve
-        signal_filt[w_window_half:-w_window_half] = get_middle_values(signal_filt_tmp, signal.shape[0], filt_window)
+        signal_filt[w_window_half:-w_window_half] = get_middle_values(signal_filt_tmp, signal.shape[0], filt_window)[w_window_half:-w_window_half]
 
     return signal_filt
 